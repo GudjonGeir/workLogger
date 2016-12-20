b313ac9702c9721138430164bdc64de98c3241b2
Guðjón Geir <gudjongeir@gmail.com>
2016-12-20 23:59:18 +0000

    Implemented method to extract issue number from toggl description

--- workLogger.py
+++ workLogger.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import re
 
 def loadTogglDay():
 	url = "https://toggl.com/reports/api/v2/details"
@@ -10,9 +11,8 @@ def loadTogglDay():
 	payload = {
 		"user_agent" : "gudjongeir@gmail.com",
 		"workspace_id" : "1487773",
-		"since" : datetime.datetime.now().strftime("%Y/%m/%d") # TODO: Optional parameter
+		"since" : datetime.datetime.now().strftime("%Y-%m-%d") # TODO: Optional parameter
 	}
-
 
 	response = requests.get(url, params=payload, auth=auth)
 
@@ -24,9 +24,27 @@ def loadTogglDay():
 	# Filter out logs that are tagged with 'logged'
 	return [{'description': wl['description'], 'duration': wl['dur']} for wl in worklogs if "Logged" not in wl['tags']]
 
+def processLogs(logs):
+	for log in logs:
+		desc = splitLogDescription(log['description'])
+		print(desc)
+
+def splitLogDescription(description):
+	reMatch = re.match( r'([a-zA-Z]+-[0-9]+)\s*(.*)', description)
+	issueNumber = None
+	description = description
+	if reMatch:
+		issueNumber = reMatch.group(1)
+		description = reMatch.group(2)
+	return {
+		"issueNumber" : issueNumber,
+		"description" : description
+	}
+
+
 def main():
 	togglLogs = loadTogglDay()
-	print(togglLogs)
+	processLogs(togglLogs)
 
 if __name__ == "__main__":
 	main()
