6e5b4c635b7876583a5c0a48c870201cc4632060
Guðjón Geir <gudjongeir@gmail.com>
2016-12-22 23:28:25 +0000

    created TogglLog class to contain log properties

--- workLogger.py
+++ workLogger.py
@@ -2,6 +2,45 @@ import requests
 import datetime
 import re
 
+class TogglLog:
+	"""docstring for TogglLog."""
+	def __init__(self, log):
+		descriptionSplit = self.splitLogDescription(log['description'])
+		self.description = descriptionSplit['description']
+		self.issueNumber = descriptionSplit['issueNumber']
+
+		self.durationMs = log['dur']
+		self.durationStr = self.formatDuration()
+
+	def formatDuration(self):
+		ret = ''
+		# remove ms
+		duration = self.durationMs / 1000
+
+		hour = int(duration / 3600)
+		if hour > 0:
+			ret += str(hour) + 'h'
+
+		minutes = int((duration % 3600) / 60)
+		if hour > 0 and minutes > 0:
+			ret += ' '
+		if minutes > 0:
+			 ret += str(minutes) + 'm'
+
+		return ret;
+
+	def splitLogDescription(self, description):
+		reMatch = re.match( r'([a-zA-Z]+-[0-9]+)\s*(.*)', description)
+		issueNumber = None
+		description = description
+		if reMatch:
+			issueNumber = reMatch.group(1)
+			description = reMatch.group(2)
+		return {
+			"issueNumber" : issueNumber,
+			"description" : description
+		}
+
 def loadTogglDay():
 	url = "https://toggl.com/reports/api/v2/details"
 	# url = "https://www.toggl.com/api/v8/workspaces"
@@ -22,45 +61,17 @@ def loadTogglDay():
 	worklogs = response.json()['data']
 
 	# Filter out logs that are tagged with 'logged'
-	return [{'description': wl['description'], 'duration': wl['dur']} for wl in worklogs if "Logged" not in wl['tags']]
+	return [TogglLog(wl) for wl in worklogs if "Logged" not in wl['tags']]
 
 def processLogs(logs):
 	for log in logs:
-		desc = splitLogDescription(log['description'])
-		print('Issue Number:      ' + desc['issueNumber'])
-		print('Toggl Description: ' + desc['description'])
-		print('Duration:          ' + formatDuration(log['duration']))
+		print('Issue Number:      ' + log.issueNumber)
+		print('Toggl Description: ' + log.description)
+		print('Duration:          ' + log.durationStr)
 
 		print('-----------------------------------------------')
 
-def formatDuration(duration):
-	ret = ''
-	# remove ms
-	duration = duration / 1000
 
-	hour = int(duration / 3600)
-	if hour > 0:
-		ret += str(hour) + 'h'
-
-	minutes = int((duration % 3600) / 60)
-	if hour > 0 and minutes > 0:
-		ret += ' '
-	if minutes > 0:
-		 ret += str(minutes) + 'm'
-
-	return ret;
-
-def splitLogDescription(description):
-	reMatch = re.match( r'([a-zA-Z]+-[0-9]+)\s*(.*)', description)
-	issueNumber = None
-	description = description
-	if reMatch:
-		issueNumber = reMatch.group(1)
-		description = reMatch.group(2)
-	return {
-		"issueNumber" : issueNumber,
-		"description" : description
-	}
 
 
 def main():
